bde6c4f9f84507148a0cc387a8cb568fcd2fc005
Madeline <madelinejgowan@gmail.com>
2024-03-27 10:12:05 -0700

    Building Form Functionality

--- src/app/(pages)/appointments/page.tsx
+++ src/app/(pages)/appointments/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@/styles/appoint.css';
 import {Container, Row, Col} from 'react-bootstrap';
-import { Contact } from '@/components/contact/contact';
+import Contact from '@/components/contact/contact';
 
 export default function Appointments() {
   return (
@@ -10,11 +10,6 @@ export default function Appointments() {
         <Row>
           <Col>
             <h1 className='title header'>APPOINTMENTS</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col className='txtCol'>
-            <p>Thank you for choosing 12 Monkeys Tattoo for your next tattoo experience. We are thrilled to embark on this creative journey with you! To schedule an appointment, email your artist of choice using the form provided. Be sure to check your Spam folder for our email, and if you haven't received a response, feel free to call us at (209) 839-1265 to check the status of your submission. We look forward to bringing your vision to life at 12 Monkeys Tattoo!</p>
           </Col>
         </Row>
         <Row>
@@ -29,10 +24,14 @@ export default function Appointments() {
           </ul>
           </Col>
           <Col className='col-12 col-md-6'>
-            <Contact/>
+           <Contact/>
           </Col>
         </Row>
-        
+        <Row>
+          <Col className='txtCol'>
+            <p>Thank you for choosing 12 Monkeys Tattoo for your next tattoo experience. We are thrilled to embark on this creative journey with you! To schedule an appointment, email your artist of choice using the form provided. Be sure to check your Spam folder for our email, and if you haven't received a response, feel free to call us at (209) 839-1265 to check the status of your submission. We look forward to bringing your vision to life at 12 Monkeys Tattoo!</p>
+          </Col>
+        </Row>
       </Container>
     </div>
   )
---
+++ src/components/contact/contact.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import React, {useRef} from 'react';
+import { Container, Row, Col, FormSelect, FormControl } from 'react-bootstrap';
+import emailjs from '@emailjs/browser';
+
+export default function Contact() {
+
+// fix type:any later
+const form: any = useRef();
+
+// fix type:any later
+const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs.sendForm('contact_service', 'contact_form', form.current, {
+        publicKey: '-rNcrdxFzlRuTpZ5O'
+    })
+    .then(
+        () => {
+            console.log('SUCCESS!');
+        },
+        (error) => {
+            console.log('FAILED...', error.text);
+        }
+    )
+}
+
+    return (
+        <form ref={form} onSubmit={sendEmail} className='form px-3 py-3'>
+            <Container>
+                <Row className='mb-2'>
+                    <Col>
+                        <label>Email</label>
+                        <FormControl type='email' name='user_email' />
+                    </Col>
+                </Row>
+                <Row className='mb-2'>
+                    <Col>
+                        <label>Artist</label>
+                        <FormSelect>
+                            <option>Jon Highland</option>
+                            <option>Kristal Elaine</option>
+                            <option>Ric Cardenas</option>
+                            <option>Jessica Orsua</option>
+                            <option>Danny Juge</option>
+                            <option>Ashley Hess</option>
+                        </FormSelect>
+                    </Col>
+                </Row>
+                <Row className='mb-2'>
+                    <Col>
+                        <label>Name</label>
+                        <FormControl type='text' name='user_name' />
+                    </Col>
+                </Row>
+                <Row className='mb-2'>
+                    <Col>
+                        <label>Message</label>
+                        <FormControl as='textarea' rows={3} name='message' />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <button className='subBtn w-100' type='submit' value='Send'>Submit</button>
+                    </Col>
+                </Row>
+            </Container>
+        </form>
+    )
+}
