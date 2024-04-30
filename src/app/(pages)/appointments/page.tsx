f96ca0464acffd8bb2a402f5afd3ceb966e6e74d
Madeline <madelinejgowan@gmail.com>
2024-04-30 14:12:47 -0700

    Updated Form, Functioning, Do Images Next

--- src/app/(pages)/appointments/page.tsx
+++ src/app/(pages)/appointments/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@/styles/appoint.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Contact from '@/components/contact/contact';
 
 export default function Appointments() {
@@ -12,51 +12,44 @@ export default function Appointments() {
             <h1 className='title header'>APPOINTMENTS</h1>
           </Col>
         </Row>
-          {/* d-block d-lg-none */}
-        <Row className='mt-2 mb-4'>
-          <Col className='txtCol'>
-            <p>Thank you for choosing 12 Monkeys Tattoo for your next tattoo experience. We are thrilled to embark on this creative journey with you. To schedule an appointment, please contact your artist of choice directly. You can find your artists preferred method of contact on our Artists page. If you haven't received a response, feel free to call us at (209) 839-1265. We look forward to bringing your vision to life at 12 Monkeys Tattoo.</p>
-          </Col>
-        </Row>
         <Row>
-          <Col className='col-12 guidelinesCol'>
-          <h3 className='header text-center'>GUIDELINES</h3>
-          <ul className='ul'>
-            <li>Must be 18 years or older to schedule an appointment. Government identification is required.</li>
-            <li>Please contact your artists directly.</li>
-            <li>No food or drinks allowed.</li>
-            <li>Friends and family are not allowed during appointments (will be asked to wait in the lobby).</li>
-            <li>Please be respectful of other clients and artists and silence your cellular devices.</li>
-          </ul>
+          <Col className='col-12 col-lg-6'>
+            <Col className='col-12 txtCol'>
+              <p>Thank you for choosing 12 Monkeys Tattoo for your next tattoo experience. We are thrilled to embark on this creative journey with you. To schedule an appointment, please contact your artist of choice directly. You can find your artists preferred method of contact on our Artists page. If you haven't received a response, feel free to call us at (209) 839-1265. We look forward to bringing your vision to life at 12 Monkeys Tattoo.</p>
+            </Col>
+            <Col className='col-12 guidelinesCol'>
+              <h3 className='header text-center'>GUIDELINES</h3>
+              <ul className='ul'>
+                <li>Must be 18 years or older to schedule an appointment. Government identification is required.</li>
+                <li>Please contact your artists directly.</li>
+                <li>No food or drinks allowed.</li>
+                <li>Friends and family are not allowed during appointments (will be asked to wait in the lobby).</li>
+                <li>Please be respectful of other clients and artists and silence your cellular devices.</li>
+              </ul>
+            </Col>
           </Col>
-          {/* <Col className='col-12 col-lg-6 formCol'>
-           <Contact/>
-          </Col> */}
+          <Col className='col-12 col-lg-6 formCol'>
+            <Contact />
+          </Col>
         </Row>
-          {/* d-none d-lg-block */}
-        {/* <Row className='my-5 d-none d-lg-block'>
-          <Col className='txtCol'>
-            <p>Thank you for choosing 12 Monkeys Tattoo for your next tattoo experience. We are thrilled to embark on this creative journey with you. To schedule an appointment, please contact your artist of choice directly. You can find your artists preferred method of contact on our Artists page. If you haven't received a response, feel free to call us at (209) 839-1265. We look forward to bringing your vision to life at 12 Monkeys Tattoo.</p>
-          </Col>
-        </Row> */}
         <Row className='my-5'>
           <Col className='col-6 col-sm-4 col-lg-2'>
-            <img className='artistImg' src='/jonHighland.png' alt='An image of artist and shop owner Jon Highland'/>
+            <img className='artistImg' src='/jonHighland.png' alt='An image of artist and shop owner Jon Highland' />
           </Col>
           <Col className='col-6 col-sm-4 col-lg-2'>
-            <img className='artistImg' src='/kristalElaine.png' alt='An image of artist Kristal Elaine'/>
+            <img className='artistImg' src='/kristalElaine.png' alt='An image of artist Kristal Elaine' />
           </Col>
           <Col className='col-6 col-sm-4 col-lg-2'>
-            <img className='artistImg' src='/ricCardenas.png' alt='An image of Ric Cardenas'/>
+            <img className='artistImg' src='/ricCardenas.png' alt='An image of Ric Cardenas' />
           </Col>
           <Col className='col-6 col-sm-4 col-lg-2'>
-            <img className='artistImg' src='/jessicaOrsua.png' alt='An image of artist Jessica Orsua'/>
+            <img className='artistImg' src='/jessicaOrsua.png' alt='An image of artist Jessica Orsua' />
           </Col>
           <Col className='col-6 col-sm-4 col-lg-2'>
-            <img className='artistImg' src='/dannyJuge.png' alt='An image of artist Danny Juge'/>
+            <img className='artistImg' src='/dannyJuge.png' alt='An image of artist Danny Juge' />
           </Col>
           <Col className='col-6 col-sm-4 col-lg-2'>
-            <img className='artistImg' src='/ashleyHess.png' alt='An image of artist Ashley Hess'/>
+            <img className='artistImg' src='/ashleyHess.png' alt='An image of artist Ashley Hess' />
           </Col>
         </Row>
       </Container>
--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,73 +1,133 @@
 'use client'
 
-import React, {useRef} from 'react';
-import { Container, Row, Col, FormSelect, FormControl } from 'react-bootstrap';
+import React, { useRef } from 'react';
+import { Container, Row, Col, FormGroup, FormLabel, FormSelect, FormControl, FormText, FormCheck } from 'react-bootstrap';
 import emailjs from '@emailjs/browser';
+import '@/styles/contact.css';
+import { getCldImageUrl, CldUploadButton } from 'next-cloudinary';
 
 export default function Contact() {
 
-    // jonstattoos.net for form example
-    // tattoo request form questions 1-7 and attach reference photos
-    // phone number, placement, full description of tattoo, attach reference photos, verify over 18?, 
+    // fix type:any later
+    const form: any = useRef();
 
-// fix type:any later
-const form: any = useRef();
+    // fix type:any later
+    const sendEmail = (e: any) => {
+        e.preventDefault();
 
-// fix type:any later
-const sendEmail = (e: any) => {
-    e.preventDefault();
-
-    emailjs.sendForm('contact_service', 'contact_form', form.current, {
-        publicKey: '-rNcrdxFzlRuTpZ5O'
-    })
-    .then(
-        () => {
-            console.log('SUCCESS!');
-        },
-        // fix type:any later
-        (error: any) => {
-            console.log('FAILED...', error.text);
-        }
-    )
-}
+        emailjs.sendForm('contact_service', 'contact_form', form.current, {
+            publicKey: '-rNcrdxFzlRuTpZ5O'
+        })
+            .then(
+                () => {
+                    alert("Message Sent");
+                },
+                // fix type:any later
+                (error: any) => {
+                    alert(`Message Failed. ${error.text}`)
+                }
+            )
+    }
 
     return (
-        <form ref={form} onSubmit={sendEmail} className='form px-3 py-3'>
+        <form id='form' ref={form} onSubmit={sendEmail} className='form px-3 py-3'>
             <Container>
                 <Row className='mb-2'>
                     <Col>
-                        <label>Email</label>
-                        <FormControl type='email' name='user_email' />
-                    </Col>
-                </Row>
-                <Row className='mb-2'>
-                    <Col>
                         <label>Artist</label>
-                        <FormSelect name='artist_email'>
+                        <FormSelect name='artist_email' required>
                             <option value='12mk@sbcglobal.net'>Jon Highland</option>
                             <option value='kristalelainetattoos@gmail.com'>Kristal Elaine</option>
                             <option value='musicnartrc@yahoo.com'>Ric Cardenas</option>
                             <option value='mccrackintattoos@gmail.com'>Jessica Orsua</option>
                             <option value='djuge01@gmail.com'>Danny Juge</option>
                             <option value='fawn715@gmail.com'>Ashley Hess</option>
+                            <option value='madelinejgowan@gmail.com'>Maddie</option>
                         </FormSelect>
                     </Col>
                 </Row>
                 <Row className='mb-2'>
                     <Col>
-                        <label>Name</label>
-                        <FormControl type='text' name='user_name' />
+                        <label>First Name</label>
+                        <FormControl required type='text' name='user_fname' />
+                    </Col>
+                    <Col>
+                        <label>Last Name</label>
+                        <FormControl required type='text' name='user_lname' />
                     </Col>
                 </Row>
                 <Row className='mb-2'>
                     <Col>
-                        <label>Message</label>
-                        <FormControl as='textarea' rows={3} name='message' />
+                        <label>Email</label>
+                        <FormControl required type='email' name='user_email' />
                     </Col>
                 </Row>
-                <Row>
+                <Row className='mb-2'>
                     <Col>
-                        <button className='subBtn w-100' type='submit' value='Send'>Submit</button>
+                        <label>Phone</label>
+                        <FormControl required type='text' name='user_phone' />
+                    </Col>
+                </Row>
+                <Row className='mb-2'>
+                    <Col>
+                        <label>Reason for Interest</label>
+                        <FormSelect name='tattoo_reason' required>
+                            <option value='New tattoo'>New tattoo</option>
+                            <option value='Complete an existing tattoo'>Complete an existing tattoo</option>
+                        </FormSelect>
+                    </Col>
+                </Row>
+                <Row className='mb-2'>
+                    <Col>
+                        <label>Location of Tattoo</label>
+                        <FormSelect name='tattoo_location' required>
+                            <option value='Shoulder'>Shoulder</option>
+                            <option value='Sleeve'>Full Sleeve</option>
+                            <option value='Bicep'>Bicep</option>
+                            <option value='Forearm'>Forearm</option>
+                            <option value='Hand'>Hand(s)</option>
+                            <option value='Ribcage'>Ribcage</option>
+                            <option value='Sternum'>Sternum</option>
+                            <option value='Stomach'>Stomach</option>
+                            <option value='Back'>Back</option>
+                            <option value='Hip'>Hip</option>
+                            <option value='Leg'>Full Leg</option>
+                            <option value='Thigh'>Thigh</option>
+                            <option value='Calf'>Calf</option>
+                            <option value='Shin'>Shin</option>
+                            <option value='Foot'>Foot/Feet</option>
+                            <option value='Other'>Other (explain)</option>
+                        </FormSelect>
+                    </Col>
+                </Row>
+                <Row className='mb-2'>
+                    <Col>
+                        <label>Full Description of Tattoo</label>
+                        <FormControl as='textarea' rows={3} name='tattoo_description' required />
+                    </Col>
+                </Row>
+                {/* <Row className='mb-1'>
+                    <Col>
+                        <FormGroup controlId="formFileMultiple1" className="mb-3">
+                            <FormLabel>Reference Photo(s)</FormLabel>
+                            <FormControl type="file" multiple name='reference_photos' required />
+                            <FormText className='formtxt'>Max 50kb total, please zip files. Include photos of area you want tattooed.</FormText>
+                        </FormGroup>
+                    </Col>
+                </Row> */}
+                {/* <Row className='mb-1'>
+                    <Col>
+                        <CldUploadButton uploadPreset="ml_default" />
+                    </Col>
+                </Row> */}
+                <Row className='mb-2'>
+                    <Col>
+                        <FormCheck required type="checkbox" label="I verify that I am over the age of 18" />
+                    </Col>
+                </Row>
+                <Row className='my-3'>
+                    <Col>
+                        <button className='subBtn w-100' type='submit' value='submit'>Submit</button>
                     </Col>
                 </Row>
             </Container>
