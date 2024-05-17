983cfcc9dbe890d39048d54f5c67a47c1e4bef56
Madeline <madelinejgowan@gmail.com>
2024-05-17 15:08:03 -0700

    Testing Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer/footer";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics/>
       <body className={inter.className}>
         <img src="/bgMonkey.png" className='bgMonkey' alt="One of two background images. A large teal version of the 12 Monkeys Tattoos logo, in the top right of the screen." />
         <img src="/bgWaves.png" className='bgWaves' alt="The second of two background images. A large teal drawing of 'Finger Waves', in the bottom left of the screen." />
